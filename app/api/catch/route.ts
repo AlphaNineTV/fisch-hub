dc2142d9b258d85d79d916c5d612313eeed4675e
AlphaNineTV <tkinqx@gmail.com>
2026-03-06 00:25:43 +0100

    fix api structure

--- app/api/catch/route.ts
+++ app/api/catch/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
+export async function GET() {
+  return NextResponse.json({ status: "API running" });
+}
+
 export async function POST(req: Request) {
   const data = await req.json();
 
